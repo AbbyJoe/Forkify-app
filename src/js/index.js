578b767c3c6f5eb610119facf1bfad43ef1bd838
Arigbede Abiodun <arigbedeabiodun@gmail.com>
2020-09-07 23:41:40 +0100

    pagination

--- src/js/index.js
+++ src/js/index.js
@@ -33,3 +33,12 @@ if(elements.seacrhForm) {
         controlSearch();
     })
 }
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage)
+    }
+})
